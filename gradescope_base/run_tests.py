54adfe810b5b5d168a850ae714acfb74d23b3b6d
Michael Noukhovitch <mnoukhov@gmail.com>
2019-10-02 17:09:24 -0400

    fixing possible student print issue
    if a student makes a print statement at import time it ruins the stdout
    capturing of gradescope (it's not a valid json anymore). This capturing
    of print statements at import time fixes that

--- gradescope_base/run_tests.py
+++ gradescope_base/run_tests.py
@@ -1,9 +1,14 @@
 import argparse
+import io
+import sys
 import unittest
 
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 
 if __name__ == '__main__':
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
     suite = unittest.defaultTestLoader.discover('.')
+    sys.stdout = original_stdout
     JSONTestRunner(visibility='visible').run(suite)
