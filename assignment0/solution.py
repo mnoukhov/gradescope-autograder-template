bbd528efd9364aff69a5d8dd8fb5e747ec42fa15
Michael Noukhovitch <mnoukhov@gmail.com>
2020-01-15 23:40:58 -0500

    write to json instead of stdout, buffer=False

--- assignment0/solution.py
+++ assignment0/solution.py
@@ -3,7 +3,7 @@ class Basics:
         return a + b
 
     def subtract(self, a, b):
-        return a - c
+        return a - b
 
     def multiply(self, a, b):
         return a * b
--- gradescope_base/run_tests.py
+++ gradescope_base/run_tests.py
@@ -1,14 +1,10 @@
 import argparse
-import io
-import sys
 import unittest
 
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 
 if __name__ == '__main__':
-    original_stdout = sys.stdout
-    sys.stdout = io.StringIO()
     suite = unittest.defaultTestLoader.discover('.')
-    sys.stdout = original_stdout
-    JSONTestRunner(visibility='visible').run(suite)
+    with open('/autograder/results/results.json', 'w') as f:
+        JSONTestRunner(visibility='visible', stream=f).run(suite)
